d3028e3de5fac0a830367e806f18734931b1dc45
Kathia Cuevas Apaza <kcuevasap@gmail.com>
2026-05-27 19:44:59 -0500

    Funcion fractalTree implementada segùn la sugerencia

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ CC = gcc
 CFLAGS = -Wall -Wextra -std=c11 -I.
 LDLIBS = -lcsfml-graphics -lcsfml-window -lcsfml-system -lm
 
-all: testLine
+all: testLine fractalTree
 
 testLine: turtlec.c examples/testLine.c turtlec.h
 	$(CC) $(CFLAGS) turtlec.c examples/testLine.c -o testLine $(LDLIBS)
 
+fractalTree: turtlec.c examples/fractalTree.c turtlec.h
+	$(CC) $(CFLAGS) turtlec.c examples/fractalTree.c -o fractalTree $(LDLIBS)
+
 clean:
-	rm -f testLine
+	rm -f testLine fractalTree
---
+++ examples/fractalTree.c
@@ -0,0 +1,42 @@
+#include "../turtlec.h"
+
+void fractalTree(Turtle *, float, int);
+
+int main(void){
+  TurtleApp *app = turtleAppCreate(1600, 800, "Fractal Tree");
+
+  if(app == NULL)
+    return 1;
+
+  Turtle *t = turtleAppGetTurtle(app);
+
+  turtlePenUp(t);
+  turtleGoTo(t, 800.0f, 750.0f);
+  turtlePenDown(t);
+
+  turtleLeft(t, 90.0);
+
+  turtleSetColor(t, 61, 242, 192);
+  turtleSetSpeed(t, 5.0f);
+  fractalTree(t, 200.0f, 4);
+
+  turtleAppRun(app);
+  turtleAppDestroy(app);
+  return 0;
+}
+
+void fractalTree(Turtle *turtle, float length, int depth){
+  if(depth == 0 || length < 5)
+    return;
+
+  turtleForward(turtle, length);
+
+  turtleLeft(turtle, 30.0);
+  fractalTree(turtle, length * 0.7, depth - 1);
+
+  turtleRight(turtle, 60.0);
+  fractalTree(turtle, length * 0.7, depth - 1);
+
+  turtleLeft(turtle, 30.0);
+  turtleBackward(turtle, length);
+}
