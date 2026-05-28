57a4ef6198328d7e970ff252336228b7d229ae72
Kathia Cuevas Apaza <kcuevasap@gmail.com>
2026-05-27 20:26:31 -0500

    cambio en nivel de profundidad: nivel 7

--- examples/fractalTree.c
+++ examples/fractalTree.c
@@ -18,7 +18,7 @@ int main(void){
 
   turtleSetColor(t, 61, 242, 192);
   turtleSetSpeed(t, 5.0f);
-  fractalTree(t, 150.0f, 5);
+  fractalTree(t, 200.0f, 7);
 
   turtleAppRun(app);
   turtleAppDestroy(app);
