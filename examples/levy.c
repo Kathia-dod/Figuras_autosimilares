316d03fcbef62dba93fd69ea2d4e579c437ee027
Kathia Cuevas Apaza <kcuevasap@gmail.com>
2026-05-29 19:35:54 -0500

    curva de Lévy: profundidad excedida(20), no se visualiza

--- examples/levy.c
+++ examples/levy.c
@@ -19,7 +19,7 @@ int main(void){
 
   turtleSetColor(t, 209, 90, 205);
   turtleSetSpeed(t, 5.0f);  
-  levy(t, 300.0f, 10);
+  levy(t, 400.0f, 20);
 
   turtleAppRun(app);
   turtleAppDestroy(app);
